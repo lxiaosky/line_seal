8ec911ff88fb85e07f7be2ac538dc801cf5acf39
mr.liang <278547958@qq.com>
2018-09-28 12:49:07 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class='demo'>
+      <Draw></Draw>
+      <Cachet></Cachet>
+      <PrivateChop></PrivateChop>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cachet from './components/cachet.vue'
+import Draw from './components/draw.vue'
+import PrivateChop from './components/privateChop.vue'
+
+export default {
+  components: {
+    Cachet,
+    Draw,
+    PrivateChop
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  #app {
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background white
+  }
+
+  .demo
+    display flex
+    justify-content space-around
+
+  .main
+    text-align center
+    margin-top 40px
+
+</style>
